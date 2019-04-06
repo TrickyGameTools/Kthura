85557a26bfb0f008276644bb670bad67a027d808
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-06 22:48:40 +0200

    Code Generated By Visual Studio

---
+++ Source/KthuraEdit/Kthura_Start.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KthuraEdit
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Kthura_Start
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Kthura_EditCore())
+                game.Run();
+        }
+    }
+#endif
+}
