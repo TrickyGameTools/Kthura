2f2f7d85030393731d2accada918eff263e80e3c
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-09 14:16:56 +0200

    Rotation calc + TagMap mapper

--- Source/Class/KthuraCore.cs
+++ Source/Class/KthuraCore.cs
@@ -10,13 +10,55 @@ namespace NSKthura {
         readonly public KthuraLayer Parent;
         public int x,y;
         public int w, h;
-        public int insertx, inserty;
-        public int R, G, B;
+        public int insertx = 0, inserty = 0;
+        public int R=255, G=255, B=255;
         public int ScaleX, ScaleY;
+        public int Dominance = 20;        
         public float TrueScaleX => (float)ScaleX / 1000;
         public float TrueScaleY => (float)ScaleY / 1000;
         public string Labels = "";
-        public string Tag = "";
+        string _Tag = "";
+        public string Tag { get => _Tag; set {
+                _Tag = value;
+                if (Kthura.automap) Parent.RemapTags();
+            }
+        }
+
+        #region recalc values
+        public int Alpha255=255;
+        public int Alpha1000 {
+            // Due to BlitzMax settings I had to use a 1=max scale, as I needed Kthura not to suffer from rouding errors, the 1000 scale has been set up.
+            // Now MonoGame uses the (actual) 0-255 scale, but as Kthura maps do not cover that properly, this measure was taken.
+            get {
+                float A = Alpha255;
+                return (int)((A / 255) * 1000);
+            }
+            set {
+                Alpha255 = (int)((value / 1000) * 255);
+                if (Alpha255 < 0) Alpha255 = 0;
+                if (Alpha255 > 255) Alpha255 = 0;
+            }
+        }
+
+        double _rotrad = 0;
+        int _rotdeg = 0;
+        double RotationRadians {
+            set {
+                _rotrad = value;
+                _rotdeg = (int)(value * (180 / Math.PI));
+            }
+            get => _rotrad;
+        }
+        int RotationDegrees {
+            set {
+                _rotdeg = value;
+                _rotrad = ((double)value * (180 / Math.PI));
+            }
+        }
+        
+
+        #endregion
+
         public KthuraObject(string objectkind, KthuraLayer prnt) {
             kind = objectkind;
             Parent = prnt;
@@ -32,10 +74,38 @@ namespace NSKthura {
         public KthuraLayer(Kthura hufter) {
             Parent = hufter ?? throw new Exception("What the....... do you think you're doing???");
         }
+
+        #region Remap
+        public void RemapTags() {
+            TagMap.Clear();
+            foreach(KthuraObject o in Objects) {
+                if (o.Tag != "") {
+                    var ok = true;
+                    if (o.Tag != o.Tag.Trim()) { ok = false; Kthura.Log($"RemapTags: \"{o.Tag}\": invalid tag!"); }
+                    if (TagMap.ContainsKey(o.Tag) { ok = false; Kthura.Log($"RemapTags: \"{o.Tag}\": duplicate tag!"); }
+                    if (ok) TagMap[o.Tag] = o;
+                }
+            }
+        }
+        #endregion
     }
 
+        delegate void KthuraLog(string l);
+
+    /// <summary>
+    /// The class containing Kthura core features and some specific data about a Kthura map.
+    /// </summary>
     class Kthura {
+        #region debug
+       static public KthuraLog Log = delegate (string m) { Debug.Print(m); };
+        #endregion
+
         #region Data specific to the map!
+        /// <summary>
+        /// When set to true, values requiring remapping (such as Tags, Lables, Dominance, BlockMapValues and (believe it or not) the y coordinate) will immediately cause that to happen.
+        /// Please note that that if you have a lot of modifications to do, this will take away performance, so then you can best turn it off, do your modifications and remap everything later.
+        /// </summary>
+        static public bool automap = true;
         public TJCRDIR TextureJCR;
         public SortedDictionary<string, KthuraLayer> Layers = new SortedDictionary<string, KthuraLayer>();
         #endregion
