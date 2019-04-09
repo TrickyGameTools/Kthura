3eca112cc7c2414540b48af8425e7e0780f85623
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-09 23:32:42 +0200

    Template Lua script

---
+++ Assets/Script/BasisScript.lua
@@ -0,0 +1,10 @@
+--[[
+
+Base Script.
+This script contains just a few quick functions all project 
+scripts written in Lua should contain. Since I do not want to write
+everything in the underlying APIs.
+
+]]
+
+local Test = "Hello world" -- The actual true script comes later!
