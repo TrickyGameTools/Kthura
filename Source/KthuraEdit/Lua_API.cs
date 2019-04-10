9f60557a0c4ff0c2ec4baa93524c0fbd32806043
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-10 11:37:44 +0200

    Concept Lua API

---
+++ Source/KthuraEdit/Lua_API.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KthuraEdit.Stages;
+
+namespace KthuraEdit
+{
+
+    // This class will not really been used by the Kthura editor itself
+    // But merely serve in order to 
+    class Lua_API {
+        public void KthuraPrint(string content) => DBG.Log(content);
+    }
+}
