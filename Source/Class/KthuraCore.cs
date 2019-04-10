c16a33eee4444a55f56fdb3316f47850aa46ce69
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-10 13:56:03 +0200

    License blocks

--- Source/Class/KthuraCore.cs
+++ Source/Class/KthuraCore.cs
@@ -1,7 +1,30 @@
+// Lic:
+// Class/KthuraCore.cs
+// Kthura Core in C#
+// version: 19.04.10
+// Copyright (C)  Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+
+
 using System.Collections.Generic;
 using System.Diagnostics;
 using System;
 using UseJCR6;
+
+
 
 namespace NSKthura { 
 
@@ -26,6 +49,7 @@ namespace NSKthura {
                 if (Kthura.automap) Parent.RemapTags();
             }
         }
+
         public int x {
             get => _x;
             set {
@@ -33,18 +57,17 @@ namespace NSKthura {
                 if (Kthura.automap) Parent.RemapDominance();
             }
         }
+
         public int y {
             get => _y;
-            set
-            {
+            set {
                 _y = value;
                 if (Kthura.automap) Parent.RemapDominance();
             }
         }
         public int Dominance {
             get => _Dominance;
-            set
-            {
+            set {
                 _Dominance = value;
                 if (Kthura.automap) Parent.RemapDominance();
             }
@@ -57,6 +80,8 @@ namespace NSKthura {
             }
         }
 
+
+
         public bool Impassible {
             get => _impassible;
             set {
@@ -65,52 +90,87 @@ namespace NSKthura {
             }
         }
 
+
+
         public bool ForcePassible {
             get => _forcepassible;
             set {
                 _forcepassible = value;
                 if (Kthura.automap) Parent.BuildBlockMap();
             }
-
         }
+
+
 
         public string DomMapVal => $"{Dominance.ToString("D9")}.{y.ToString("D9")}.{x.ToString("D9")}.{cnt.ToString("D9")}";
         // Dominance takes prioity over all. When domincance is the same then y will play a role, and then the x value.
         // cnt just has to make sure every value is unique.
 
-
         #region recalc values
+
         public int Alpha255=255;
+
         public int Alpha1000 {
+
             // Due to BlitzMax settings I had to use a 1=max scale, as I needed Kthura not to suffer from rouding errors, the 1000 scale has been set up.
+
             // Now MonoGame uses the (actual) 0-255 scale, but as Kthura maps do not cover that properly, this measure was taken.
+
             get {
+
                 float A = Alpha255;
+
                 return (int)((A / 255) * 1000);
+
             }
+
             set {
+
                 Alpha255 = (int)((value / 1000) * 255);
+
                 if (Alpha255 < 0) Alpha255 = 0;
+
                 if (Alpha255 > 255) Alpha255 = 0;
+
             }
+
         }
+
+
 
         double _rotrad = 0;
+
         int _rotdeg = 0;
+
         double RotationRadians {
+
             set {
+
                 _rotrad = value;
+
                 _rotdeg = (int)(value * (180 / Math.PI));
+
             }
+
             get => _rotrad;
+
         }
+
         int RotationDegrees {
+
             set {
+
                 _rotdeg = value;
+
                 _rotrad = ((double)value * (180 / Math.PI));
+
             }
+
         }
+
         
+
+
 
         #endregion
 
@@ -119,9 +179,10 @@ namespace NSKthura {
             Parent = prnt;
             cnt = Parent.cnt;
             Parent.cnt++;
-
         }
     }
+
+
 
     class KthuraLayer {
         internal int cnt = 0; 
@@ -133,6 +194,8 @@ namespace NSKthura {
         public KthuraLayer(Kthura hufter) {
             Parent = hufter ?? throw new Exception("What the....... do you think you're doing???");
         }
+
+
 
         #region Remap
         public void RemapTags() {
@@ -147,6 +210,7 @@ namespace NSKthura {
             }
         }
 
+
         public void RemapDominance() {
             // This was (for now) the easiest way to go, but maybe not the most optimal.
             // If you think you got a faster method... Lemme know!
@@ -156,6 +220,8 @@ namespace NSKthura {
             ObjectDrawOrder.Clear();
             foreach (KthuraObject o in DominanceMap.Values) ObjectDrawOrder.Insert(0, o);
         }
+
+
 
         public void RemapLabels() {
             LabelMap.Clear();
@@ -176,80 +242,118 @@ namespace NSKthura {
             }
         }
 
+
         public void BuildBlockMap() { /* TODO: Code the damn blockmap builder! */}
         #endregion
     }
 
+
         delegate void KthuraLog(string l);
+
 
     /// <summary>
     /// The class containing Kthura core features and some specific data about a Kthura map.
     /// </summary>
     class Kthura {
         #region debug
+
        static public KthuraLog Log = delegate (string m) { Debug.Print(m); };
+
         #endregion
 
         #region Data specific to the map!
         /// <summary>
+
         /// When set to true, values requiring remapping (such as Tags, Lables, Dominance, BlockMapValues and (believe it or not) the y coordinate) will immediately cause that to happen.
+
         /// Please note that that if you have a lot of modifications to do, this will take away performance, so then you can best turn it off, do your modifications and remap everything later.
+
         /// </summary>
         static public bool automap = true;
         public TJCRDIR TextureJCR;
+
         public SortedDictionary<string, KthuraLayer> Layers = new SortedDictionary<string, KthuraLayer>();
+
         #endregion
 
         #region Control Methods
-        public void CreateLayer(string name) {
-            if (Layers.ContainsKey(name)) {
-                Debug.Print($"Kthura map already has a layer named {name}. Please pick a different name!");
-                return;
-            }
-            Layers[name] = new KthuraLayer(this);
-        }
-        #endregion
 
+        public void CreateLayer(string name) {
+
+            if (Layers.ContainsKey(name)) {
+
+                Debug.Print($"Kthura map already has a layer named {name}. Please pick a different name!");
+
+                return;
+
+            }
+
+            Layers[name] = new KthuraLayer(this);
+
+        }
+
+        #endregion
 
         #region New Map constructors
         private Kthura() { } // I won't allow "New". A bit dangerous. So some static functions act as "constructors".
         public static Kthura Create() => Create(DefaultTextureJCR);
         public static Kthura Create(TJCRDIR TexJCR) {
+
             var ret = new Kthura();
+
             ret.TextureJCR = TexJCR;
+
             // Please note! Single layer maps, were already officially deprecated a few years before this project began in C#
+
             // And if BlitzMax didn't get into the state it's now, this C# version may actually never have been created (at least not by me)
+
             // It's only logical I will not support it any more in the C# version :P
+
             ret.Layers["_BASE"] = new KthuraLayer(ret);
 
+
+
             // All done
+
             return ret;
+
         }
 
         public static Kthura Load(TJCRDIR sourcedir,string prefix,TJCRDIR TexJcr)  {
+
             // This is the TRUE load routine. All overloads eventually lead to this one! ;-P
+
             throw new System.Exception("Kthura.Load: This routine is not yet available!");
+
         }
 
         public static Kthura Load(string sourcefile,string prefix, TJCRDIR TexJCR) {
+
             var sf = JCR6.Dir(sourcefile);
+
             return Load(sf, prefix, TexJCR);
+
         }
 
         public static Kthura Load(TJCRDIR sourcedir, string prefix="") {
+
             var tj = DefaultTextureJCR;
+
             if (tj == null) tj = sourcedir;
+
             return Load(sourcedir, prefix, tj);
+
         }
 
         public static Kthura Load(string sourcefile, string prefix = "") {
+
             var sf = JCR6.Dir(sourcefile);
+
             return Load(sf, prefix);
+
         }
+
         #endregion
-
-
-
 
         #region Core functions as statics (since C# requires classes even when you don't need them).
         static TJCRDIR DefaultTextureJCR = null;
@@ -258,3 +362,4 @@ namespace NSKthura {
     }
 
 }
+
--- Source/KthuraEdit/Kthura_Start.cs
+++ Source/KthuraEdit/Kthura_Start.cs
@@ -1,4 +1,30 @@
-﻿using System;
+// Lic:
+// Kthura for C#
+// Start
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.04.10
+// EndLic
+
+ï»¿using System;
 
 namespace KthuraEdit
 {
@@ -20,3 +46,4 @@ namespace KthuraEdit
     }
 #endif
 }
+
