5ef582d99094f0850201528a49ad0d38a7a5e9ef
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-10 13:30:53 +0200

    ME

--- Source/KthuraEdit/Lua_API.cs
+++ Source/KthuraEdit/Lua_API.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using KthuraEdit.Stages;
+using NSKthura;
 
 namespace KthuraEdit
 {
@@ -11,6 +12,11 @@ namespace KthuraEdit
     // This class will not really been used by the Kthura editor itself
     // But merely serve in order to 
     class Lua_API {
+
+        // Should Replace Lua's own print command
         public void KthuraPrint(string content) => DBG.Log(content);
+
+        // When creating new CSpots, the "ME" object should contain the Kthura object in question.
+        public KthuraObject ME;
     }
 }
