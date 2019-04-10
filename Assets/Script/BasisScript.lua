03687a6ab6c13940dc060d288c9b7fddb1169eed
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-10 11:37:34 +0200

    Print in basis script

--- Assets/Script/BasisScript.lua
+++ Assets/Script/BasisScript.lua
@@ -7,4 +7,23 @@ everything in the underlying APIs.
 
 ]]
 
-local Test = "Hello world" -- The actual true script comes later!
+
+-- Replacing Lua's own "print" command
+local LUATRUEPRINT=print
+function print(a,b,c,d,e,f)
+   params={a,b,c,d,e,f}
+   content=""
+   for a in ipairs(params) do
+       content = content .. a
+   end
+   KthuraAPI.KthuraPrint(content)
+end
+local print=print -- locals are faster than globals
+
+
+
+
+
+-- Destroy import function for safety reasons
+import = function() end
+print("Lua script loaded succesfully")
