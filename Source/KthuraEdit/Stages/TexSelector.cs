2f7af6ac14a91d04a2a7fce0dea6885d311653de
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-15 23:09:19 +0200

    Texture selector

---
+++ Source/KthuraEdit/Stages/TexSelector.cs
@@ -0,0 +1,165 @@
+// Lic:
+// Kthura in C#
+// Texture Selector
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 19.04.13
+// EndLic
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TrickyUnits;
+using UseJCR6;
+
+namespace KthuraEdit.Stages
+{
+    internal struct TexItem{
+        public int y;
+        public string name;
+        public TQMGText text;
+        public TexItem(int ny, string nname) { y = ny; name = nname;text = UI.font20.Text(nname); }
+    }
+
+    class TexSelector:BaseStage {
+
+        #region Static HardCore
+        static TexSelector me = new TexSelector();
+        static public void ComeToMe()        {
+            Core.GoStage(me);
+        }
+        #endregion
+
+        #region Overrides
+        public override void Draw() {
+            GetTextures();
+            ReloadTextures();
+            ShowTexture();
+            ListTextures();
+            AutoChecks();
+        }
+
+        public override void Update() {
+            if (TQMGKey.Hit(Microsoft.Xna.Framework.Input.Keys.Escape)) MainEdit.ComeToMe();
+        }
+        #endregion
+
+        #region stuff
+        bool GoToTab = true;
+        bool TabData = true;
+        List<TexItem> Textures;
+        int time2reload = 250;
+        TQMGImage LoadedTex = UI.back;
+        string chosen="";
+        int ScrollY = 0;
+        int W => (int)(UI.ScrWidth * .75);
+        TQMGText tGoToTab = UI.font20.Text("Go to last used object kind");
+        TQMGText tTabData = UI.font20.Text("Retreive last used object data");
+
+        void GetTextures() {
+            if (Textures == null) {
+                Textures = new List<TexItem>();
+                var y = 0;
+                var lastbundle = "";
+                foreach (TJCREntry ent in Core.Map.TextureJCR.Entries.Values) {
+                    var e = qstr.ExtractExt(ent.Entry);
+                    if (e.ToUpper() == "PNG") {
+                        var dirsplit = ent.Entry.Split('/');
+                        var bundle = -1;
+                        for(int i=0; i<dirsplit.Length;i++) {
+                            var d = dirsplit[i];
+                            if (bundle < 0 && qstr.Suffixed(d.ToUpper(), ".JPBF")) bundle = i;
+                        }
+                        if (bundle < 0) {
+                            Textures.Add(new TexItem(y, ent.Entry)); y += 25;
+                        } else if (lastbundle=="" || !qstr.Prefixed(ent.Entry,lastbundle)) {
+                            lastbundle = "";
+                            for (int i=0;i<=bundle;i++) {
+                                if (lastbundle != "") lastbundle += "/";
+                                lastbundle += dirsplit[i];
+                                
+                            }
+                            Textures.Add(new TexItem(y, lastbundle)); y += 25;
+                        }
+                    }
+                }
+            }
+        }
+
+        void ReloadTextures() {
+            if (time2reload > 0) {
+                time2reload--;
+                if (time2reload<=0) {
+                    if (chosen == "")
+                        LoadedTex = UI.back;
+                    else {
+                        var v = Core.Map.TextureJCR.AsMemoryStream(chosen);
+                        if (v == null) { DBG.Log($"Failure on loading texture: '{chosen}': {JCR6.JERROR}"); LoadedTex = UI.back; return; }
+                        var qs = new QuickStream(v);
+                        LoadedTex = TQMG.GetImage(qs);
+                        if (LoadedTex == null) LoadedTex = UI.back;
+                    }
+                }
+            }
+        }
+
+        void ShowTexture() {
+            // This color change was done to make sure the screen text would remain readable!
+            // It's only a quick preview after all
+            TQMG.Color(180, 0, 255);
+            if (LoadedTex != null) TQMG.SimpleTile(LoadedTex, 0, 0, UI.ScrWidth, UI.ScrHeight);
+        }
+
+        void ListTextures() {
+            foreach(TexItem TI in Textures) {                
+                if (Core.ms.X < W && Core.ms.Y > TI.y && Core.ms.Y < TI.y + 22) {
+                    if (chosen != TI.name) time2reload = 250;
+                    chosen = TI.name;
+                    if (Core.MsHit(1)) {
+                        UI.SetTexture(TI.name, GoToTab, TabData);
+                        MainEdit.ComeToMe();
+                    }
+                }
+                TQMG.Color(0, 255, 255);
+                if (TI.name == chosen) {
+                    TQMG.DrawRectangle(0, TI.y, W, 21);
+                    TQMG.Color(0, 0, 0);
+                }
+                TI.text.Draw(5, TI.y - ScrollY);
+            }
+        }
+
+        void AutoChecks() {
+            TQMG.Color(0, 255, 255);
+            UI.CheckboxImage[GoToTab].Draw(W, 20);
+            UI.CheckboxImage[TabData].Draw(W, 40);
+            tGoToTab.Draw(W + 22, 20);
+            tTabData.Draw(W + 22, 40);
+            if (Core.MsHit(1) && Core.ms.X>W) {
+                if (Core.ms.Y > 20 && Core.ms.Y < 40) GoToTab = !GoToTab;
+                if (Core.ms.Y > 40 && Core.ms.Y < 60) TabData = !TabData;
+            }
+        }
+        
+        #endregion
+    }
+}
+
