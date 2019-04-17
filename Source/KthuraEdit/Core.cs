017eb1ed0ae61acd647623603c6d014c508ea23a
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-17 19:06:39 +0200

    vUpadte

--- Source/KthuraEdit/Core.cs
+++ Source/KthuraEdit/Core.cs
@@ -21,8 +21,9 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.04.16
+// Version: 19.04.17
 // EndLic
+
 
 
 
@@ -301,6 +302,7 @@ namespace KthuraEdit
         #endregion
     }
 }
+
 
 
 
