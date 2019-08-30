a79715a5a04e90c653ee9e9a5ae827b194297fb2
Jeroen P. Broks <facebook@tbbs.nl>
2019-08-30 11:33:15 +0200

    vUpdate

--- Source/KthuraBubble/KthuraBubble.cs
+++ Source/KthuraBubble/KthuraBubble.cs
@@ -21,8 +21,9 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.08.27
+// Version: 19.08.30
 // EndLic
+
 
 
 
@@ -541,6 +542,7 @@ namespace KthuraBubble {
     }
 
 }
+
 
 
 
