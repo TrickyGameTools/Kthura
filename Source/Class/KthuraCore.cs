b13b0ceae62f9ad96f9a9e32592068b7a7509f1d
Jeroen P. Broks <facebook@tbbs.nl>
2020-05-25 17:18:48 +0200

    vUpdate

--- Source/Class/KthuraCore.cs
+++ Source/Class/KthuraCore.cs
@@ -1,8 +1,8 @@
 // Lic:
 // Class/KthuraCore.cs
 // Kthura Core in C#
-// version: 19.11.23
-// Copyright (C)  Jeroen P. Broks
+// version: 20.05.25
+// Copyright (C) 2019 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
@@ -961,22 +961,3 @@ namespace NSKthura {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
