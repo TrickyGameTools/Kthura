9df2393258dc0ccc722442526c074d0fd9383346
Jeroen Broks <facebook@tbbs.nl>
2021-03-28 13:26:08 +0200

    Basic C++ Code
    (Will (for now) only show build and copyright data. The rest of the code comes later).

---
+++ Source/KthuraEdit/src/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main(int aantal_arg, char** arg) {
+	using namespace std;
+	cout << "Kthura Map Editor - Build " <<  __TIMESTAMP__ << "\n";
+	cout << "Coded by: Tricky\n";
+	cout << "(c) 2021 Jeroen P. Broks - Released under the terms of the GPL3\n\n";
+}
