70abb7aa303cf769fc97fb46c5347ae50de2a9d9
Jeroen Broks <facebook@tbbs.nl>
2021-04-01 22:37:40 +0200

    License block

--- Source/KthuraEdit/src/Config.cpp
+++ Source/KthuraEdit/src/Config.cpp
@@ -1,3 +1,28 @@
+// Lic:
+// Kthura Map Editor (C++)
+// Configuration
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2021
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 21.04.01
+// EndLic
 // Myself
 #include "../headers/Config.hpp"
 
@@ -13,7 +38,7 @@ using namespace TrickyUnits;
 #define CLIERR(err) std::cout << "\x7\x1b[31mCLI ERROR>\x1b[0m\t"<<err<<endl
 
 #ifdef CONFIGDEBUG
-#define ConfChat(abs) std::cout << "DENUG>\t" << abs<<endl
+#define ConfChat(abs) std::cout << "DEBUG>\t" << abs<<endl
 #else
 #define ConfChat(abs)
 #endif
