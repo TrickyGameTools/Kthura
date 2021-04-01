5dbd3f9ff50a0f01ae67a5bac15a96ee274c4830
Jeroen Broks <facebook@tbbs.nl>
2021-04-01 12:54:36 +0200

    User Interface (header)
    (more is to come, doncha worry).

---
+++ Source/KthuraEdit/headers/UserInterface.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "../headers/Config.hpp"
+
+
+namespace KthuraEdit {
+	class UI {
+	private:
+	public:
+		static void Start();
+		static bool Run();
+		static void Done();
+	};
+}
