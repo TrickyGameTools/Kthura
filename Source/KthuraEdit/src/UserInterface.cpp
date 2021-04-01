088eb1bcb90d7f077fbe2cced8bf46a5a36d5277
Jeroen Broks <facebook@tbbs.nl>
2021-04-01 15:38:48 +0200

    Check

--- Source/KthuraEdit/src/UserInterface.cpp
+++ Source/KthuraEdit/src/UserInterface.cpp
@@ -4,15 +4,23 @@
 // C++
 #include <iostream>
 
+// Units
+#include <TQSE.hpp>
+#include <TQSG.hpp>
+
 
 namespace KthuraEdit {
+	bool UI::_initialized{ false };
+
 	void UI::Start() {
+		_initialized = true;
 		std::cout << "Staring User Interface\n";
 	}
 	bool UI::Run() {
 		return false; // True code comes later!
 	}
 	void UI::Done() {
+		_initialized = false;
 		std::cout << "Closing User Interface\n";
 	}
 }
