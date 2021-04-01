90830b8745dcd20164bea8042227bdef580ee056
Jeroen Broks <facebook@tbbs.nl>
2021-04-01 12:54:50 +0200

    UserInterface - Skeleton

---
+++ Source/KthuraEdit/src/UserInterface.cpp
@@ -0,0 +1,18 @@
+// self
+#include "../headers/UserInterface.hpp"
+
+// C++
+#include <iostream>
+
+
+namespace KthuraEdit {
+	void UI::Start() {
+		std::cout << "Staring User Interface\n";
+	}
+	bool UI::Run() {
+		return false; // True code comes later!
+	}
+	void UI::Done() {
+		std::cout << "Closing User Interface\n";
+	}
+}
