e16f242f62671125a2e1994918003d27a9151755
Jeroen Broks <facebook@tbbs.nl>
2021-04-04 01:17:05 +0200

    Preview Gadget

--- Source/KthuraEdit/src/UI_TexSelect.cpp
+++ Source/KthuraEdit/src/UI_TexSelect.cpp
@@ -1,7 +1,24 @@
 #include <june19.hpp>
+#include "../headers/UserInterface.hpp"
 #include "../headers/UI_TexSelect.hpp"
 
+using namespace june19;
+using namespace TrickyUnits;
 
+namespace KthuraEdit {
+	static j19gadget
+		*Preview{ nullptr },
+		*TexList{ nullptr },
+		*UsedTexList{nullptr},
+		*Okay{ nullptr },
+		*Cancel{ nullptr };
 
-void KthuraEdit::GoToTex(june19::j19gadget* g, june19::j19action a) {
+	static void CreateSelector() {
+		UI::AddStage("PicTex");
+		auto prnt = UI::GetStage("PicText")->MainGadget;
+		CreatePicture(TQSG_ScreenWidth() - 300,0,300,prnt->H(),prnt);
+	}
+
+	void GoToTex(june19::j19gadget* g, june19::j19action a) {
+	}
 }
