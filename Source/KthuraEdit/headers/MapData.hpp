eb2a94d40f373016047e0c7c6851f2eee77031b1
Jeroen Broks <facebook@tbbs.nl>
2021-04-03 09:27:01 +0200

    Evil globals, muhuhahahahahaha!

---
+++ Source/KthuraEdit/headers/MapData.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Kthura.hpp>
+
+namespace KthuraEdit{
+
+	extern NSKthura::Kthura WorkMap;
+	extern int ScrollX;
+	extern int ScrollY;
+
+}
---
+++ Source/KthuraEdit/src/MapData.cpp
@@ -0,0 +1,10 @@
+#include "../headers/MapData.hpp"
+namespace KthuraEdit {
+
+
+	// Yes, I know! Globals are evil! I don't care!
+	NSKthura::Kthura WorkMap;
+	int ScrollX=0;
+	int ScrollY=0;
+
+}
