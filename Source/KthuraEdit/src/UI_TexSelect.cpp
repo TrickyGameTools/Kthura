26c919960fdc920e28c990398f75e7135032f4c7
Jeroen Broks <facebook@tbbs.nl>
2021-04-04 01:44:36 +0200

    Magic number voidance

--- Source/KthuraEdit/src/UI_TexSelect.cpp
+++ Source/KthuraEdit/src/UI_TexSelect.cpp
@@ -6,6 +6,9 @@ using namespace june19;
 using namespace TrickyUnits;
 
 namespace KthuraEdit {
+
+	static const int ListMargin = 200;
+
 	static j19gadget
 		* Preview{ nullptr },
 		* TexList{ nullptr },
@@ -21,12 +24,13 @@ namespace KthuraEdit {
 		UI::AddStage("PicTex");
 		auto prnt = UI::GetStage("PicText")->MainGadget;
 		Preview = CreatePicture(TQSG_ScreenWidth() / 2, 0, TQSG_ScreenWidth() / 2, prnt->H(), prnt);
-		TexList = CreateListBox(0, 0, prnt->W()-200, prnt->H(), prnt);
+		TexList = CreateListBox(0, 0, prnt->W()-ListMargin, prnt->H(), prnt);
 		TexList->SetBackground(0, 0, 0, 0);
 		TexList->SetForeground(0, 255, 255, 255);
-		UsedTexList = CreateListBox(0, 0, prnt->W() - 200, prnt->H(), prnt);
+		UsedTexList = CreateListBox(0, 0, prnt->W() - ListMargin, prnt->H(), prnt);
 		UsedTexList->SetBackground(0, 0, 0, 0);
 		UsedTexList->SetForeground(255, 180, 0, 255);
+
 	}
 
 	void GoToTex(june19::j19gadget* g, june19::j19action a) {
