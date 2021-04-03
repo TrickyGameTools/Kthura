0b9d47ce8529ab25a7ade1d410a8ef4d1a9c238b
Jeroen Broks <facebook@tbbs.nl>
2021-04-03 11:30:48 +0200

    vUpdate and missing license blocks added
    (and a few cases of Unixification (a word I invented myself. Means that all line ends are converted to Unix style, so LF only)).

--- Source/KthuraEdit/src/Main.cpp
+++ Source/KthuraEdit/src/Main.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 21.04.01
+// Version: 21.04.03
 // EndLic
 // C++
 #include <iostream>
--- Source/KthuraLauncher/MainConfig.cs
+++ Source/KthuraLauncher/MainConfig.cs
@@ -4,7 +4,7 @@
 // 
 // 
 // 
-// (c) Jeroen P. Broks, 
+// (c) Jeroen P. Broks, 2019
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 19.04.11
+// Version: 21.04.03
 // EndLic
 
 
@@ -107,5 +107,3 @@ namespace Kthura
 
     }
 }
-
-
