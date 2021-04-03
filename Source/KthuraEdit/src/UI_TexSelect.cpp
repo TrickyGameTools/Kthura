737cc6f2c81e6cc8fece6bfffd830e42a825d2ec
Jeroen Broks <facebook@tbbs.nl>
2021-04-04 01:26:56 +0200

    Need

--- Source/KthuraEdit/src/UI_TexSelect.cpp
+++ Source/KthuraEdit/src/UI_TexSelect.cpp
@@ -7,16 +7,20 @@ using namespace TrickyUnits;
 
 namespace KthuraEdit {
 	static j19gadget
-		*Preview{ nullptr },
-		*TexList{ nullptr },
-		*UsedTexList{nullptr},
-		*Okay{ nullptr },
-		*Cancel{ nullptr };
+		* Preview{ nullptr },
+		* TexList{ nullptr },
+		* UsedTexList{ nullptr },
+		* Okay{ nullptr },
+		* Cancel{ nullptr },
+		* ButtonChainType{ nullptr },
+		* ButtonTexData{ nullptr },
+		* ButtonUsedOnly{ nullptr };
+
 
 	static void CreateSelector() {
 		UI::AddStage("PicTex");
 		auto prnt = UI::GetStage("PicText")->MainGadget;
-		CreatePicture(TQSG_ScreenWidth() - 300,0,300,prnt->H(),prnt);
+		CreatePicture(TQSG_ScreenWidth() - 300, 0, 300, prnt->H(), prnt);
 	}
 
 	void GoToTex(june19::j19gadget* g, june19::j19action a) {
