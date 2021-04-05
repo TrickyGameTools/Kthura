5577bebdea02b2b5332181231cc7da4ad5947c94
Jeroen Broks <facebook@tbbs.nl>
2021-04-05 21:17:53 +0200

    Fix: Texture not chosen on "Okay"

--- Source/KthuraEdit/src/UI_TexSelect.cpp
+++ Source/KthuraEdit/src/UI_TexSelect.cpp
@@ -1,3 +1,28 @@
+// Lic:
+// Kthura Map Editor (C++)
+// Texture selector
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2021
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 21.04.05
+// EndLic
 #include <june19.hpp>
 #include "../headers/UserInterface.hpp"
 #include "../headers/UI_TexSelect.hpp"
@@ -31,11 +56,12 @@ namespace KthuraEdit {
 			std::string tab{ "" };
 			// TODO: Go to correct tab *if* applicable
 			SetTex(e->ItemText(),tab);
+			TQSE_Flush();
 			UI::GoToStage("Map");
 		}
 	}
 	static void ActOkay(j19gadget* g, j19action a) {
-		if (UsedTexList)
+		if (UsedTexList->Visible)
 			DClTexList(UsedTexList, j19action::DoubleClick);
 		else
 			DClTexList(TexList, j19action::DoubleClick);
