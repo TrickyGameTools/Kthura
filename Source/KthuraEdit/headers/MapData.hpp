705f18b331fd761fb5eb78d77018d51093cf7ff2
Jeroen Broks <facebook@tbbs.nl>
2021-04-09 11:08:17 +0200

    vUpdate

--- Source/KthuraEdit/headers/MapData.hpp
+++ Source/KthuraEdit/headers/MapData.hpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 21.04.05
+// Version: 21.04.09
 // EndLic
 #pragma once
 
