dcb38670f131e9d96f037179aac662da601334a6
Jeroen Broks <facebook@tbbs.nl>
2021-04-12 20:00:20 +0200

    Empty stuff, but more will come

---
+++ Source/KthuraEdit/src/Script.cpp
@@ -0,0 +1,22 @@
+// Lua
+#include "../headers/QuickLuaInclude.hpp"
+#include "../headers/Script.hpp"
+
+// Kthura
+#include "../headers/Config.hpp"
+
+// Stuff
+#include <jcr6_core.hpp>
+
+using namespace jcr6;
+
+static lua_State* LState{ nullptr };
+static JT_Dir LDir;
+
+
+namespace KthuraEdit {
+
+	void InitScript() {
+	}
+
+}
