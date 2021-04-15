d209616866fcdbcae7b3eab947590649d36b3070
Jeroen Broks <facebook@tbbs.nl>
2021-04-15 11:41:33 +0200

    Lua

--- Source/KthuraEdit/dll-copy.ps1
+++ Source/KthuraEdit/dll-copy.ps1
@@ -12,6 +12,7 @@
 $sdl = "\Projects\Applications\VisualStudio\VC\3rdparty\SDL2-2.0.12"
 $sdl_image = "E:\Projects\Applications\VisualStudio\VC\3rdparty\SDL2_image-2.0.5"
 $zlib = "E:\projects\applications\visualstudio\vc\Apollo Game Engine\packages\zlib-msvc14-x64.1.2.11.7795\build\native\bin_release\" # Copied from my Apollo project
+$lua = "E:/Projects/Applications/VisualStudio/VC/3rdparty/Lua/lua-5.4.0/Lua Library/x64/Release"
 
 
 echo "Copying SDL2"
@@ -22,6 +23,9 @@ cp "$sdl_image\lib\x64\*.dll" "x64\Release"
 
 echo "Copying zlib"
 cp "$zlib\zlib.dll" "x64\Release"
+
+echo "Copying Lua"
+cp "$lua/Lua.dll" "x64/Release"
 
 
 echo "Need assets"
