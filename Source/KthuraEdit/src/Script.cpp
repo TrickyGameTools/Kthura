c22d575e5b64b03b84a55ec8588cf05e3149841a
Jeroen Broks <facebook@tbbs.nl>
2022-01-06 00:18:49 +0100

    vUpdate

--- Source/KthuraEdit/src/Script.cpp
+++ Source/KthuraEdit/src/Script.cpp
@@ -4,7 +4,7 @@
 // 
 // 
 // 
-// (c) Jeroen P. Broks, 2021
+// (c) Jeroen P. Broks, 2021, 2022
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 21.07.25
+// Version: 22.01.06
 // EndLic
 
 
