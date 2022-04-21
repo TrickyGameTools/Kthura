8fc8762d363a385a99656dffdf67ea2b4338fc57
Jeroen Broks <facebook@tbbs.nl>
2022-04-21 11:14:19 +0200

    vUpdate

--- Source/Class/KthuraSave.cs
+++ Source/Class/KthuraSave.cs
@@ -1,8 +1,8 @@
 // Lic:
 // Class/KthuraSave.cs
 // Save Kthura for C#
-// version: 22.03.26
-// Copyright (C) 2019 Jeroen P. Broks
+// version: 22.04.21
+// Copyright (C) 2019, 2022 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
 // arising from the use of this software.
